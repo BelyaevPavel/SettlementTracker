8d038dfe6845f090ab25c76e5bb23dff49770177
BelyaevPavel <gbelbih@gmail.com>
2026-02-26 21:21:08 +0500

    refactor: services are moved from WebInterface to Core
    - BuildingService, IBuildingService, CitizenService, IBuildingService, SettlementResourcesService, ISettlementResourcesService
    - usings, references and namespaces are fixed accordingly

---
+++ SettlementTracker.Core/Services/ICitizenService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SettlementTracker.Core.Models.Entities;
+
+namespace SettlementTracker.Core.Services
+{
+    public interface ICitizenService
+    {
+        Task<IEnumerable<Citizen>> GetCitizensAsync();
+        Task<Citizen?> GetCitizenAsync(Guid id);
+        Task AddCitizenAsync(Citizen citizen);
+        Task UpdateCitizenAsync(Citizen citizen);
+        Task DeleteCitizenAsync(Guid id);
+        Task SaveChangesAsync();
+        Task LoadFromFileAsync();
+    
+        public event EventHandler? CitizensChange;
+    }
+}
---
+++ SettlementTracker.Core/Services/ISettlementResourcesService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SettlementTracker.Core.Managers;
+using SettlementTracker.Core.Models.Definitions;
+
+namespace SettlementTracker.WebInterface.Data.Services
+{
+    public interface ISettlementResourcesService
+    {
+        /// <summary>
+        /// Loads JSON from standard file path.
+        /// </summary>
+        /// <returns></returns>
+        Task LoadDefinitionsAsync();
+
+        IReadOnlyList<ResourceDefinition> GetResourceDefinitions();
+        IReadOnlyDictionary<string, float> GetCurrentBalance();
+
+
+        Task AddResourceAsync(string resourceId, float amount);
+    
+        Task<bool> TrySpendResourceAsync(string resourceId, float amount);
+    
+        bool CanSpend(string resourceId, float amount);
+        event EventHandler<ResourcesChangedEventArgs> ResourcesChanged;
+        Task ApplyDailyEffectsAsync(IEnumerable<ResourceEffect> dailyEffects);
+    }
+}
