92060aa79be7524e80decec684fefcfd9f4a2be1
BelyaevPavel <gbelbih@gmail.com>
2026-03-12 23:06:46 +0500

    feat: basic implementation of IStatefulEntity

---
+++ SettlementTracker.Core/Services/IStatefulEntity.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace SettlementTracker.Core.Services
+{
+    /// <summary>
+    /// Определяет контракт для сущностей, состояние которых необходимо сохранять и загружать.
+    /// </summary>
+    public interface IStatefulEntity
+    {
+        /// <summary>
+        /// Асинхронно сохраняет состояние сущности.
+        /// </summary>
+        /// <param name="cancellationToken">Токен отмены операции.</param>
+        /// <returns>Задача, представляющая асинхронную операцию сохранения.</returns>
+        Task SaveStateAsync(CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Асинхронно загружает состояние сущности.
+        /// </summary>
+        /// <param name="cancellationToken">Токен отмены операции.</param>
+        /// <returns>Задача, представляющая асинхронную операцию загрузки.</returns>
+        Task LoadStateAsync(CancellationToken cancellationToken = default);
+    }
+}
